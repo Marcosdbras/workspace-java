d0eebd18cad625c0bfdfbf63e550c567c659a8b2
marcosbras <marcosbras@hotmail.com>
2017-07-19 23:17:42 -0300

    Classes.java > como configurar github

--- programClasses/src/programClasses/Classes.java
+++ programClasses/src/programClasses/Classes.java
@@ -4,6 +4,12 @@ public class Classes {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		/* Como configurar github
+		 * 
+		 * https://www.youtube.com/watch?v=aEGkYK6AVRg
+		 * 
+		 * */
 
 	}
 
