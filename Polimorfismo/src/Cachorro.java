1b35099512d7ca3fd2f23df544bb1ac1809fd48e
marcosbras <marcosbras@hotmail.com>
2017-07-24 23:38:40 -0300

    Heranca e Construtor

---
+++ Polimorfismo/src/Cachorro.java
@@ -0,0 +1,16 @@
+
+public class Cachorro extends Animal{
+
+	@Override
+	public void falar() {
+		// TODO Auto-generated method stub
+		
+		System.out.println("AU-AU");
+		
+	}
+	
+	public void morder() {
+		System.out.println("NHAC");		
+	}
+
+}
